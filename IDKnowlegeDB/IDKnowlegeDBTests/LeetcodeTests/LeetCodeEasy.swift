3bb29dc1a0f98a94a63000b87d3278a671843623
Aleksey Borisov <irdisdevil@gmail.com>
2024-06-28 06:06:43 +0300

    -(update) first task

--- IDKnowlegeDB/IDKnowlegeDBTests/LeetcodeTests/LeetCodeEasy.swift
+++ IDKnowlegeDB/IDKnowlegeDBTests/LeetcodeTests/LeetCodeEasy.swift
@@ -10,28 +10,38 @@ import XCTest
 @testable import IDKnowlegeDB
 
 final class LeetCodeEasy: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testLongestCommonPrefix() throws {
+    let arr1 = ["flower","flow","flight"]
+    let arr2 = ["dog","racecar","car"]
+    
+    XCTAssertEqual(longestCommonPrefix(arr1), "fl")
+    XCTAssertEqual(longestCommonPrefix(arr2), "")
+  }
+  
+  func testPerformanceExample() throws {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  private func longestCommonPrefix(_ strs: [String]) -> String {
+    guard let firstWord = strs.first else { return "" }
+    var prefix = firstWord
+    
+    while strs.filter({ $0.hasPrefix(prefix) }).count != strs.count && !prefix.isEmpty {
+        prefix.removeLast()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
+    return prefix
+}
 }
