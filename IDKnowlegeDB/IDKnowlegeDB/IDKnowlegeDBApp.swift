94f354d0b0f05bcb3c5c3c75ee414affdad4f782
Aleksey Borisov <irdisdevil@gmail.com>
2024-06-28 05:03:10 +0300

    - (add) project

---
+++ IDKnowlegeDB/IDKnowlegeDB/IDKnowlegeDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDKnowlegeDBApp.swift
+//  IDKnowlegeDB
+//
+//  Created by Alexey on 28.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct IDKnowlegeDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
